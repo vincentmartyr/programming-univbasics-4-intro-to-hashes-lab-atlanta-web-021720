ac4d6bc25b54badc6eedd79d9c5fa14bf46f2d34
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-07 17:36:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,9 +39,9 @@ end
 def update_counting_hash(hash, key)
 
 if hash[key]
-    p hash[key] += 1
+    p {hash[key] += 1}
 else
-    p hash[key] = 1
+    p {hash[key] = 1}
 end
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
