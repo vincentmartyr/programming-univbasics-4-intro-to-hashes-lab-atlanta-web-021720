69a91cfa5ae859fb81286f84b29293c6c839da45
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-07 16:24:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,8 +25,8 @@ end
 
 def my_hash_creator(key, value)
 
-fresh_hash = {"key" => "value"}
-  # return a hash that includes the key and value parameters passed into this method
+fresh_hash = {key => value}
+
 end
 
 def read_from_hash(hash, key)
